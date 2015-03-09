c75f8d53b18b6cacf4ee74e411bf0b0d585ba921
Vivian Li <vivian2938@gmail.com>
2015-03-09 13:16:07 -0400

    began quicksort

--- 06QuickSelect/Sorts.java
+++ 06QuickSelect/Sorts.java
@@ -2,11 +2,10 @@ import java.util.*;
 import java.io.*;
 
 public class Sorts{
-
-    Random rand = new Random();
     
     public static int quickSelect(int[] ary, int si, int ei, int index){
 	int[] d = new int[ary.length];
+	Random rand = new Random();
 	
 	int start = si;
 	int end = ei;
@@ -39,36 +38,6 @@ public class Sorts{
     }
 
     public static void quicksort(int[] arr){
-	int[] d = new int[ary.length];
-	
-	int start = si;
-	int end = ei;
-	int pivot = ary[rand.nextInt(ei - si + 1) + si];
-	
-	for(int i = si; i <= ei; i++){
-	    if(ary[i] < pivot){
-		d[start] = ary[i];
-		start++;
-	    }else if(ary[i] > pivot){
-		d[end] = ary[i];
-		end++;
-	    }
-	}
-	int keep;
-	d[start] = pivot;
-	if(start == index){
-	    keep = d[start];
-	} else{
-	    System.arraycopy(d, 0, ary, 0, d.length);
-	    if(index < start){
-		keep = quickSelect(ary, si, start - 1, index);
-	    }
-	    else{
-		keep = quickSelect(ary, start + 1, ei, index);
-	    }
-	    
-	}
-	return keep;		
     }
 	
 
@@ -84,6 +53,6 @@ public class Sorts{
 	}
 	
 	System.out.println(Arrays.toString(a));
-	
+	System.out.println(quickSelect(a, 0, 10, 2));
     }
 }
