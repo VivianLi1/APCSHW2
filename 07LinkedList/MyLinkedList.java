53b06157feb5e832ef1caae468d51965b40e9201
VivianLi1 <vivian2938@gmail.com>
2015-03-12 13:09:58 -0400

    toString and getSize methods

--- 07LinkedList/MyLinkedList.java
+++ 07LinkedList/MyLinkedList.java
@@ -4,10 +4,37 @@ public class MyLinkedList{
     LNode head;
     
     public String toString(){
-	String s = head;
-	s += " ";
+	LNode temp;
+	String s = "[ ";
+	//current = head;
+	temp = head;
+	while(temp != null){
+	    s += temp.getValue() + ", ";
+	    temp = temp.getNext();
+	}
+	return s.substring(0, s.length() - 1) + " ]";
     }
 
+    public int getSize(){
+	int counter = 0;
+	while(current != null){
+	    counter++;
+	}
+	return counter;
+    }
+    
+    public void add(int value){
+	if(current == null){
+	    head.setValue(value);
+	}
+    }
+    
+    public static void main(String[] args){
+	MyLinkedList test = new MyLinkedList();
+	test.add(10);
+	System.out.println(test.toString());
+	System.out.println(test.getSize());
+    }
 }
 
     
