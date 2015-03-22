8789351fc11c6182d4c3f2733c915fcf97166d1d
Vivian Li <vivian2938@gmail.com>
2015-03-22 15:00:21 -0400

    finished MyStack

--- 09Stack/MyStack.java
+++ 09Stack/MyStack.java
@@ -2,15 +2,39 @@ import java.util.*;
 
 public class MyStack<T>{
 
+    private LinkedList<T> current = new LinkedList<T>();
+
     public boolean empty(){
+	return current.size() == 0;
     }
 
     public T peek(){
+	if(empty()){
+	    throw new EmptyStackException();
+	}else{
+	    return current.getFirst();
+	}
     }
 
     public T pop(){
+	if(empty()){
+	    throw new EmptyStackException();
+	}else{
+	    T temp = current.getFirst();
+	    current.remove();
+	    return temp;
+	}
     }
 
     public T push(T o){
+	current.add(0,o);
+	return o;
     }
+    /*
+    public static void main(String[] args){
+	MyStack<Integer> test = new MyStack<Integer>();
+	test.push(1);
+	System.out.println(test.peek());
+    }
+    */
 }
---
+++ 09Stack/MyStack.java~
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class MyStack<T>{
+
+    public boolean empty(){
+    }
+
+    public T peek(){
+    }
+
+    public T pop(){
+    }
+
+    public T push(T o){
+    }
+}
