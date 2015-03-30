deefdbd90ba2af65f55e08ca321caf67246b2254
Vivian Li <vivian2938@gmail.com>
2015-03-30 11:51:10 -0400

    added more methods and stuff

--- 10DEQueue/MyDeque.java
+++ 10DEQueue/MyDeque.java
@@ -4,33 +4,80 @@ import java.io.*;
 public class MyDeque<T>{
 
     private Object[] arr;
-    private int head, tail;
-    private int size = 0;
+    private int head, tail, size;
+
+    public MyDeque(){
+	arr = new Object[10];
+	size = 0;
+	head = 0;
+	tail = head + (size - 1);
+    }
     
     public void addFirst(T value){
-	
+	if(size == arr.length){
+	    resize(arr.length*2);
+	}
+	head--;
+	if(head == -1){
+	    head = arr.lengt - 1;
+	}
+	arr[head] = value;
+	size++;
     }
 
     public void addLast(T value){
+	if(size == arr.length){
+	    resize(arr.length * 2);
+	}
+	if(tail > arr.lengt){
+	    tail = 0;
+	}
+	arr[tail] = value;
+	size++;
+	    
     }
-
-    /*
+    
     public T removeFirst(){
+	T temp = arr[head];
+	arr[head] = null;
+	head++;
+	return (T)temp;
     }
 
     public T removeLast(){
+	T temp = arr[tail];
+	arr[tail] = null;
+	tail--;
+	return (T)temp;
     }
-
+    
+    
     public T getFirst(){
+	return (T)arr[head];
     }
 
     public T getLast(){
+	return (T)arr[tail];
     }
-    */
 
-    public void resize(){
-	Object[] copy = Arrays.copyOf(arr, size * 2);
-	arr = copy;
+    public void resize(int newSize){
+	Object[] copy = new Object[newSize];
+	if(head < tail){
+	    for(int i = head; i <= tail; i++){
+		copy[i] = arr[i];
+	    }
+	    else{
+		for(int i = head; i < arr.length; i++){
+		    copy[i] = arr[i];
+		}
+		for(int i = 0; i <= tail; i++){
+		    copy[i+ arr.length] = arr[i];
+		}
+		tail += arr.length;
+	    }
+	    arr = copy;
+	}
+	
     }
 
 }
