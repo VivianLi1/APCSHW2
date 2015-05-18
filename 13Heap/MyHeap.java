c0f822e6fd9c3b93b9cf5c33ffa4d0d2e63329ab
Vivian Li <vivian2938@gmail.com>
2015-05-17 22:27:48 -0400

    pushup method

--- 13Heap/MyHeap.java
+++ 13Heap/MyHeap.java
@@ -20,7 +20,7 @@ public class MyHeap{
     }
 
     public String toString(){
-	return "";
+	return arr.toString();
     }
 
     public int remove(){
@@ -28,52 +28,63 @@ public class MyHeap{
     }
 
     public void add(int num){
-	if(arr[0] == 0){
-	    arr[1] = num;
-	}
-	int ind = emptyIndex();
-	arr[ind] = num;
-	int i = ind;
-	while(arr[i] < arr[i - 1]){
-	    arr[i - 1] = arr[i];
-	    arr[i] = arr[i - 1];
-	    i--;
+	resize();
+	arr[0] = arr[0] + 1;
+	arr[arr[0]] = num;
+        if(isMax){
+	    pushUp();
+	}else{
+	    pushDown();
 	}
     }
-    
+
+    public void pushUp(){
+	int lastInd = arr[0];
+	int last = arr[lastInd];
+	int parentLast = arr[getParentInd(lastInd)];
+        while(parentLast > last){
+	    swap(lastInd);
+	    lastInd = getParentInd(lastInd);
+	    last = arr[lastInd];
+	    parentLast = arr[getParentInd(lastInd)];
+	}
+    }
+
+    public void pushDown(){
+    }
 
     public void resize(){
-	if(arr[0] == arr.length - 1){
+	if(arr[0] >= arr.length - 1){
 	    arr = Arrays.copyOf(arr, arr[0] * 2);
 	}
     }
-
-    public boolean compare(int i, int j){
-	if(isMax){
-	    return arr[i] > arr[j];
-	}
-	else{
-	    return arr[i] < arr[j];
-	}
-    }
-    
+    /*
+      public boolean compare(int i, int j){
+      if(isMax){
+      return arr[i] > arr[j];
+      }
+      else{
+      return arr[i] < arr[j];
+      }
+      }
+    */
     public void swap(int ind){
-	int parent = getParent(ind);
+	int parent = getParentInd(ind);
 	int t = arr[ind];
 	arr[ind] = arr[parent];
 	arr[parent] = t;
     }
 
-    public void getLeft(int ind){
+    public int getLeftInd(int ind){
 	return ind * 2;
     }
 
-    public void getRight(int ind){
+    public int getRightInd(int ind){
 	return ind * 2 + 1;
     }
 
-    public void getParent(int node){
-	return node / 2;
+    public int getParentInd(int ind){
+	return ind / 2;
     }
     
     public int peek(){
@@ -81,16 +92,21 @@ public class MyHeap{
     }
     
     /*
-    private int emptyIndex(){
-	int i = 1;
-	while(arr[i] != null){
-	    i++;
-	}
-	return i;
-    }
+      private int emptyIndex(){
+      int i = 1;
+      while(arr[i] != null){
+      i++;
+      }
+      return i;
+      }
     */
 	
     public static void main(String[] args){
+	MyHeap testMax = new MyHeap();
+	MyHeap testMin = new MyHeap(false);
+
+	testMax.add(10);
+	System.out.println(testMax);
     }
 }
 
